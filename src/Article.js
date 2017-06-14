a6db52a71b5426dcfbe1044845cab086c546540b
Ilya Toka <ilyatoka@yahoo.com>
2017-06-14 11:11:50 +0300

    add CommentList to each Article

--- src/Article.js
+++ src/Article.js
@@ -1,32 +1,47 @@
 import React, {Component} from 'react'
+import CommentList from './CommentList'
 
 export default class Article extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            isOpen: true
+            isOpen: false
         }
     }
 
     render() {
         const {article} = this.props
-        const {isOpen} = this.state
         return (
             <div>
                 <h3>{article.title}</h3>
-                <button onClick = {this.toggleOpen}>
-                    {isOpen ? 'close' : 'open'}
-                </button>
-                {this.getBody()}
+                {this.getControls()}
+                {this.getContent()}
+                {this.getComments()}
             </div>
         )
     }
 
-    getBody() {
+    getControls() {
+        const {isOpen} = this.state
+        return (
+            <button onClick = {this.toggleOpen}>
+                {isOpen ? 'close' : 'open'}
+            </button>
+        )
+    }
+
+    getContent() {
         if (!this.state.isOpen) return null
         const {article} = this.props
         return <section>{article.text}</section>
+    }
+
+    getComments() {
+        if (!this.state.isOpen) return null
+        const {article} = this.props
+        const comments = article.comments
+        return comments && <section><CommentList comments = {comments}/></section>
     }
 
     toggleOpen = (ev) => {
