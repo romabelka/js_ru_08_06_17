1636d7edc36bb261222fd1586054ef12e4b4ae7c
Roman <kaboom.ua@gmail.com>
2017-06-19 12:14:15 +0300

    Update toggleOpenArticle.js

--- src/decorators/toggleOpenArticle.js
+++ src/decorators/toggleOpenArticle.js
@@ -1,8 +1,10 @@
 import React, {Component as ReactComponent} from 'react'
 
+//WrappedComponent я называл чтоб легче понять было. Лучше выбирай более значущее название
 export default (OriginalComponent) => class WrappedComponent extends ReactComponent {
 
   state = {
+      //Не привязывайся к названиям сущностей, вся суть декораторов в универсальности. Сделай openItemId
       openArticleId: null
   }
 
