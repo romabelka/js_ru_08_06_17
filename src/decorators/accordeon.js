eb3bcb279e9a4d28ce15f6fe3181da7657a48e72
Roman <kaboom.ua@gmail.com>
2017-06-19 12:02:44 +0300

    Update accordeon.js

--- src/decorators/accordeon.js
+++ src/decorators/accordeon.js
@@ -2,10 +2,12 @@ import React, {Component as ReactComponent} from 'react'
 
 export default (AccordionComponent) => class ReadyAccComponent extends ReactComponent {
     state = {
+        //Не привязывайся к названиям сущностей, вся суть декораторов в универсальности. Сделай openItemId
         openArticleId: null
     }
 
     render() {
+        //перевод строки после return мешает - в результате ты возвращаяешь undefined
         return 
             <AccordionComponent 
                 {...this.props} 
