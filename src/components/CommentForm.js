96ec37f948ef8822c7e7d835b0f8b20c2ba48a81
asapovk <asapovk@gmail.com>
2017-06-21 19:08:24 +0300

    added colored input functional

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
+import './commentForm.css'
 
 export default class CommentFrom extends Component {
     state = {
       userInput: '',
       textInput: ''
+    }
+    showColor (input) {
+      if(input.length < 5 || input.length >= 15 ) return 'red'
     }
     handleUserInput = ev => {
       this.setState({userInput: ev.target.value})
@@ -14,8 +18,8 @@ export default class CommentFrom extends Component {
     }
     render () {
       return (<div>
-        <input type='text' value = {this.state.userInput} onChange={this.handleUserInput}/>
-        <input type='text' value = {this.state.textInput} onChange={this.handleTextInput}/>
+        <input type='text' value = {this.state.userInput} onChange={this.handleUserInput} className={this.showColor(this.state.userInput)}/>
+        <input type='text' value = {this.state.textInput} onChange={this.handleTextInput} className={this.showColor(this.state.textInput)}/>
         </div>)
     }
 }
