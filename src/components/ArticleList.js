a9936d074177468bb466e67c2f7e7ede4b0f35c0
Alex Krechik <alex.krechick@gmail.com>
2017-06-25 17:14:50 +0300

    Filter Articles depends on the filter values

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -8,13 +8,32 @@ class ArticleList extends Component {
     static propTypes = {
         //from connect
         articles: PropTypes.array.isRequired,
+	    selected: PropTypes.array,
+	    dateRange: PropTypes.shape({
+		    from: PropTypes.object,
+		    to: PropTypes.object
+	    }),
         //from accordion
         openItemId: PropTypes.string,
         toggleOpenItem: PropTypes.func.isRequired
     }
     render() {
-        const { articles, openItemId, toggleOpenItem } = this.props
-        const articleElements = articles.map(article => <li key={article.id}>
+        const { articles, openItemId, toggleOpenItem, selected, dateRange } = this.props
+        const articleElements = articles
+	        .filter(article => {
+		        if (!selected) return true
+	        	if (!selected.length) return true
+	        	return !!selected.find(select => select.value === article.id)
+	        })
+	        .filter(article => {
+	        	const {to, from} = dateRange
+		        const current = new Date(article.date)
+	        	if (!to) {
+			        return true
+		        }
+		        return current >= from && current <= to
+	        })
+	        .map(article => <li key={article.id}>
             <Article
                 article = {article}
                 isOpen = {article.id === openItemId}
@@ -30,6 +49,4 @@ class ArticleList extends Component {
     }
 }
 
-export default connect(state => ({
-    articles: state.articles
-}))(accordion(ArticleList))
+export default connect(({articles, selected, dateRange}) => ({articles, selected, dateRange}))(accordion(ArticleList))
