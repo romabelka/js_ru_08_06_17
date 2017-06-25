f838397e5b5af7119e01be3e9e422348060f5337
asapovk <asapovk@gmail.com>
2017-06-25 23:19:02 +0300

    enabled filter by date range

--- src/fixtures.js
+++ src/fixtures.js
@@ -1,7 +1,7 @@
 export const articles = [
     {
         "id": "56c782f18990ecf954f6e027",
-        "date": "2016-06-09T15:03:23.000Z",
+        "date": "2017-06-09T15:03:23.000Z",
         "title": "Commodo qui incididunt",
         "text": "Commodo qui incididunt ex ut ea nulla et eu aliquip duis. Aute deserunt excepteur ullamco fugiat sunt aliquip exercitation do sint incididunt. Amet consectetur sint irure reprehenderit fugiat amet mollit. In commodo mollit ullamco cillum pariatur eiusmod cillum aute mollit. Culpa non sint eiusmod ad dolor velit dolore voluptate do adipisicing. Cupidatat sint est magna officia qui magna eu elit qui excepteur fugiat duis ex labore.\n\nAliquip veniam ad reprehenderit mollit exercitation id enim ut exercitation. Esse irure ipsum minim laborum reprehenderit irure ut. Tempor excepteur nisi nulla nostrud amet id cillum. Sint velit sint officia aliqua sint quis deserunt.\n\nAliquip dolor cillum deserunt enim nulla dolor amet irure cupidatat commodo laboris id aliqua. Labore aliqua adipisicing Lorem id adipisicing. Ad cupidatat et do anim ex commodo elit magna ad consequat. Nostrud sit eu laborum ut consequat fugiat aute culpa. Lorem tempor quis sunt ad consequat excepteur est. Enim voluptate cillum Lorem ex fugiat ea qui. Irure aute magna dolore eiusmod minim non ad anim dolore sint et.",
         "comments": [
--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -1,11 +1,13 @@
 import {articles as defaultArticles} from '../fixtures'
-import {DELETE_ARTICLE} from '../constants'
+import {DELETE_ARTICLE, DATE_RANGE} from '../constants'
 
 export default (articleState = defaultArticles, action) => {
     const {type, payload} = action
 
     switch (type) {
         case DELETE_ARTICLE: return articleState.filter(article => article.id !== payload.id)
+        case DATE_RANGE: return articleState.filter(article => {if(action.dates.to) return Date.parse(article.date) >= Date.parse(action.dates.from) && Date.parse(article.date) <= Date.parse(action.dates.to)
+        else return Date.parse(article.date) >= Date.parse(action.dates.from)})
     }
 
     return articleState
