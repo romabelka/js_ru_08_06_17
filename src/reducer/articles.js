7f647e73f891ecc293b4a4dd783255578347c960
Roman <kaboom.ua@gmail.com>
2017-06-26 12:03:16 +0300

    Update articles.js

--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -6,6 +6,7 @@ export default (articleState = defaultArticles, action) => {
 
     switch (type) {
         case DELETE_ARTICLE: return articleState.filter(article => article.id !== payload.id)
+        //по сути ты удаляешь статьи, а должен хранить здесь список всех статей, а фильтровать где-то в другом месте(например в коннекте)
         case DATE_RANGE: return articleState.filter(article => {if(action.dates.to) return Date.parse(article.date) >= Date.parse(action.dates.from) && Date.parse(article.date) <= Date.parse(action.dates.to)
         else return Date.parse(article.date) >= Date.parse(action.dates.from)})
     }
