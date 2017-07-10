e2c8e205251b1c6158c0cf30a0832ebcc1a58012
Roman Iakobchuk <r.iakobchuk@gmail.com>
2017-07-10 20:15:12 +0300

    add 404 handler

--- src/AC/index.js
+++ src/AC/index.js
@@ -1,6 +1,7 @@
 import {DELETE_ARTICLE, INCREMENT, CHANGE_DATE_RANGE, CHANGE_SELECTION, ADD_COMMENT,
     LOAD_ALL_ARTICLES, LOAD_ARTICLE, LOAD_ARTICLE_COMMENTS, LOAD_COMMENTS_FOR_PAGE, START, SUCCESS, FAIL
 } from '../constants'
+import {push, replace} from 'react-router-redux'
 
 export function increment() {
     return {
@@ -61,15 +62,23 @@ export function loadArticle(id) {
 
         setTimeout(() => {
             fetch(`/api/article/${id}`)
-                .then(res => res.json())
+                .then(res => {
+                    if (res.status >= 400) {
+                        throw new Error(res.statusText)
+                    }
+                    return res.json()
+                })
                 .then(response => dispatch({
                     type: LOAD_ARTICLE + SUCCESS,
                     payload: { id, response }
                 }))
-                .catch(error => dispatch({
-                    type: LOAD_ARTICLE + FAIL,
-                    payload: { id, error }
-                }))
+                .catch(error => {
+                    dispatch({
+                        type: LOAD_ARTICLE + FAIL,
+                        payload: { id, error }
+                    })
+                    dispatch(replace('/error'))
+                })
         }, 500)
     }
 }
